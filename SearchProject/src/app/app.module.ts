8756c9be3b572b53c791afef7d1a7d7cbaf0ce06
UlyanYunakh <ulyanyunakh@gmail.com>
2021-09-09 17:50:42 +0300

    Add new components to module.

--- SearchProject/src/app/app.module.ts
+++ SearchProject/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GoogleApiService } from './google-api.service';
+import { MainComponent } from './main/main.component';
+import { AuthComponent } from './auth/auth.component';
+import { LoadingComponent } from './loading/loading.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    AuthComponent,
+    LoadingComponent
   ],
   imports: [
     BrowserModule,
