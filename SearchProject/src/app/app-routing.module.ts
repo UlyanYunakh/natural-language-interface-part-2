1d531e6bb770e656a01b6861cf3ab4e4a6a07da9
UlyanYunakh <ulyanyunakh@gmail.com>
2021-09-09 17:49:58 +0300

    Add main and singIn route

--- SearchProject/src/app/app-routing.module.ts
+++ SearchProject/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { MainComponent } from './main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'main', component: MainComponent },
+  { path: 'singIn', component: AuthComponent },
+
+  { path: '**', redirectTo: 'singIn' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
