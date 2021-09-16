778eddb745e83fdd93a739e5ceaeb7a49d30a2f1
UlyanYunakh <ulyanyunakh@gmail.com>
2021-09-16 13:43:27 +0300

    Page reload event lisener.

--- lab-1/SearchProject/src/app/app.component.ts
+++ lab-1/SearchProject/src/app/app.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
-import { AuthService } from './auth.service';
+import { Component, OnInit } from '@angular/core';
 import { GoogleApiService } from './google-api.service';
+import { filter } from 'rxjs/operators'
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   constructor(
-    private api: GoogleApiService
+    private api: GoogleApiService,
+    private router: Router,
   ) { }
+
+  ngOnInit() {
+    this.router.events
+      .pipe(filter((rs): rs is NavigationEnd => rs instanceof NavigationEnd))
+      .subscribe(event => {
+        if (event.id === 1 && event.url === event.urlAfterRedirects) {
+          this.router.navigate(["singIn"])
+        }
+      })
+  }
 }
