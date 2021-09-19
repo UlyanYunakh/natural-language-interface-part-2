dc1f93c5e62e906d766b1c1f520ec11c814e7e7c
UlyanYunakh <ulyanyunakh@gmail.com>
2021-09-19 20:40:32 +0300

    Create app info component.

---
+++ lab-1/SearchProject/src/app/app-info/app-info.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-app-info',
+  templateUrl: './app-info.component.html'
+})
+export class AppInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
