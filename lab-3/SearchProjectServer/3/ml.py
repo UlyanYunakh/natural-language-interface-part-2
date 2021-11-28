34dbd0d0a8b00fc66140fea2808820049c2c0c11
YanaGv <gv.yanina@gmail.com>
2021-11-28 19:12:57 +0300

    lab3

---
+++ lab-3/SearchProjectServer/3/ml.py
@@ -0,0 +1,75 @@
+from nltk.corpus import stopwords
+from nltk.cluster.util import cosine_distance
+import numpy as np
+import networkx as nx
+import os
+
+
+def read_doc(file_name):
+    file = open(file_name, "r",encoding="utf8")
+    filedata = file.readlines()
+    filedata = " ".join(filedata).replace("\n", "")
+    article = filedata.split(". ")
+    sentences = []
+    for sentence in article:
+        sentences.append(sentence.replace("[^a-zA-Z]", " ").split(" "))
+    sentences.pop()
+    return sentences
+
+
+def sentence_similarity(sent1, sent2, stop_words=None):
+    if stop_words is None:
+        stop_words = []
+    sent1 = [w.lower() for w in sent1]
+    sent2 = [w.lower() for w in sent2]
+    all_words = list(set(sent1+sent2))
+    vector1 = [0] * len(all_words)
+    vector2 = [0] * len(all_words)
+
+    for w in sent1:
+        if w in stop_words:
+            continue
+        vector1[all_words.index(w)] += 1
+
+    for w in sent2:
+        if w in stop_words:
+            continue
+        vector2[all_words.index(w)] += 1
+
+    return 1 - cosine_distance(vector1, vector2)
+
+
+def build_similarity_matrix(sentences, stop_words):
+    similarity_matrix = np.zeros((len(sentences), len(sentences)))
+
+    for idx1 in range(len(sentences)):
+        for idx2 in range(len(sentences)):
+            if idx1 == idx2:
+                continue
+            similarity_matrix[idx1][idx2] = sentence_similarity(
+                sentences[idx1], sentences[idx2], stop_words)
+    return similarity_matrix
+
+
+def generate_summary(file_name, lang, top_n=3):
+    stop_words = stopwords.words(lang)
+    summarize_text = []
+    sentences = read_doc(file_name)
+    smilarity_matrix = build_similarity_matrix(sentences, stop_words)
+    similarity_graph = nx.from_numpy_array(smilarity_matrix)
+    scores = nx.pagerank(similarity_graph)
+    ranked_sentences = sorted(
+        ((scores[i], s) for i, s in enumerate(sentences)), reverse=True)
+
+    for i in range(top_n):
+        summarize_text.append(" ".join(ranked_sentences[i][1]))
+
+    print(". ".join(summarize_text))
+    print("\n")
+
+
+for path in os.listdir("english_docs"):
+    generate_summary(f"english_docs/{path}", 'german')
+
+for path in os.listdir("french_docs"):
+    generate_summary(f"french_docs/{path}", 'french')
